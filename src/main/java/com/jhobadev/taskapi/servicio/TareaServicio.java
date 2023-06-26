86925cfed3c0d595789e53e4cd6e65d0a60bc005
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-25 20:20:52 -0500

    Feat: servicio para crear tarea

---
+++ src/main/java/com/jhobadev/taskapi/servicio/TareaServicio.java
@@ -0,0 +1,23 @@
+package com.jhobadev.taskapi.servicio;
+
+import com.jhobadev.taskapi.mapper.TareaInDTOaTarea;
+import com.jhobadev.taskapi.persistencia.entidad.Tarea;
+import com.jhobadev.taskapi.persistencia.repositorio.TareaRepositorio;
+import com.jhobadev.taskapi.servicio.dto.TareaInDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TareaServicio {
+
+    private TareaRepositorio tareaRepositorio;
+    private TareaInDTOaTarea tareaInDTOaTarea;
+
+    public TareaServicio(TareaRepositorio tareaRepositorio, TareaInDTOaTarea tareaInDTOaTarea) {
+        this.tareaRepositorio = tareaRepositorio;
+        this.tareaInDTOaTarea = tareaInDTOaTarea;
+    }
+
+    public Tarea crearTarea(TareaInDTO tareaInDTO) {
+        return tareaInDTOaTarea.map(tareaInDTO);
+    }
+}
