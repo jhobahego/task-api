773381c78834c74657da4a8d516b14f7a8a5b59a
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-25 21:35:14 -0500

    Feat: ruta para obtener todas las tareas

--- src/main/java/com/jhobadev/taskapi/controlador/TareaControlador.java
+++ src/main/java/com/jhobadev/taskapi/controlador/TareaControlador.java
@@ -6,6 +6,8 @@ import com.jhobadev.taskapi.servicio.dto.TareaInDTO;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/tareas")
 public class TareaControlador {
@@ -14,6 +16,11 @@ public class TareaControlador {
 
     public TareaControlador(TareaServicio tareaServicio) {
         this.tareaServicio = tareaServicio;
+    }
+
+    @GetMapping
+    public List<Tarea> obtenerTodasLasTareas() {
+        return this.tareaServicio.obtenerTodasLasTareas();
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
