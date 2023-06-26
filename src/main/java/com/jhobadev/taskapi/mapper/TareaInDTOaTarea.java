f85f99f14792241701e0c04741a867d85dce92d4
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-25 20:50:57 -0500

    Fix: anotación @Component

--- src/main/java/com/jhobadev/taskapi/mapper/TareaInDTOaTarea.java
+++ src/main/java/com/jhobadev/taskapi/mapper/TareaInDTOaTarea.java
@@ -2,7 +2,9 @@ package com.jhobadev.taskapi.mapper;
 
 import com.jhobadev.taskapi.persistencia.entidad.Tarea;
 import com.jhobadev.taskapi.servicio.dto.TareaInDTO;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TareaInDTOaTarea implements IMapper<TareaInDTO, Tarea>{
 
     @Override
