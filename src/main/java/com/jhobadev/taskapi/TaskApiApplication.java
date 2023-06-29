b9f681ea2326cdeeae9bc86771ac9360e6e4b94e
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-28 19:51:37 -0500

    Feat: Bean para manejo de cors

--- src/main/java/com/jhobadev/taskapi/TaskApiApplication.java
+++ src/main/java/com/jhobadev/taskapi/TaskApiApplication.java
@@ -2,12 +2,27 @@ package com.jhobadev.taskapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TaskApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173", "https://jhobadev-todo-app-ts.netlify.app")
+													.allowedHeaders("*")
+													.allowedMethods("*");
+			}
+		};
 	}
 
 }
