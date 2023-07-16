e7ef21eb20ac66787af1759808219f927a37f656
Jhon Hernandez <jhobahego16@hotmail.com>
2023-07-16 18:28:13 -0500

    Refactor: titulo unico

--- src/main/java/com/jhobadev/taskapi/modelos/Tarea.java
+++ src/main/java/com/jhobadev/taskapi/modelos/Tarea.java
@@ -12,6 +12,7 @@ public class Tarea {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String title;
 
     private boolean completed;
